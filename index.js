6da69f33e070e35fe798dd4f091fd9df6dac4550
Andrew Gnott <gnottandrew@gmail.com>
2017-06-05 15:02:20 -0400

    Setting up Express

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello World');
+//Setup environment variables
+require('dotenv').config();
+
+//Create general variables
+let ENV     = process.env,
+    io      = require('socket.io'),
+    express = require('express');
+
+//Create the app
+let app = express();
+
+//Set up static file sending
+app.use(express.static('public'));
+
+//Start express server
+app.listen(ENV.PORT, function(){
+  console.log(`App running on port ${ENV.PORT}`);
+});
